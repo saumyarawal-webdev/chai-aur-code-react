d9502e847e8c58016ee7c959d55b44346da02132
unknown <rawalsaumya22@gmail.com>
2024-07-24 23:46:03 +0530

    08ContextApi - 25:00

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+<UserContextProvider>
+<h1>React with Chai</h1>
+</UserContextProvider>
+   
+    </>
+  )
+}
+
+export default App
